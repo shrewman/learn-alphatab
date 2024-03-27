21c2be5e1564c3f6ed9ed23a0640128e88f0bffa
shrewman <feelswhiteman@gmail.com>
2024-03-27 13:52:04 +0200

    Controls done

--- public/index.js
+++ public/index.js
@@ -25,6 +25,11 @@ api.scoreLoaded.on((score) => {
   });
 });
 
+api.scoreLoaded.on((score)=>{
+  wrapper.querySelector('.at-song-title').innerText = score.title;
+  wrapper.querySelector('.at-song-artist').innerText = score.artist;
+});
+
 api.renderStarted.on(() => {
   const tracks = new Map();
   api.tracks.forEach((t) => {
@@ -40,3 +45,48 @@ api.renderStarted.on(() => {
     }
   });
 });
+
+const countIn = wrapper.querySelector('.at-controls .at-count-in');
+countIn.onclick = () => {
+  countIn.classList.toggle('active');
+  if (countIn.classList.contains('active')) {
+    api.countInVolume = 1;
+  } else {
+    api.countInVolume = 0;
+  }
+};
+
+const metronome = wrapper.querySelector('.at-controls .at-metronome');
+metronome.onclick = () => {
+  metronome.classList.toggle('active');
+  if (metronome.classList.contains('active')) {
+    api.metronomeVolume = 1;
+  } else {
+    api.metronomeVolume = 0;
+  }
+};
+
+const loop = wrapper.querySelector('.at-controls .at-loop');
+loop.onclick = () => {
+  loop.classList.toggle('active');
+  api.isLooping = loop.classList.contains('active');
+};
+
+const zoom = wrapper.querySelector('.at-controls .at-zoom select');
+zoom.onchange = ()=>{
+  const zoomLevel = parseInt(zoom.value) / 100;
+  api.settings.display.scale = zoomLevel;
+  api.updateSettings();
+  api.render();
+};
+
+const layout = wrapper.querySelector('.at-controls .at-layout select');
+layout.onchange = () => {
+  if (layout.value === 'horizontal') {
+      api.settings.display.layoutMode = alphaTab.LayoutMode.Horizontal;
+  } else if (layout.value === 'page') {
+      api.settings.display.layoutMode = alphaTab.LayoutMode.Page;
+  }
+  api.updateSettings();
+  api.render();
+};
